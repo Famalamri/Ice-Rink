0b0428a03fcf23e4bfb09051ac70a2731cb61bee
Famalamri <amiralamri099@gmail.com>
2024-01-19 03:26:02 +0000

    Gun Script + VFX
    Coded ability to fire gun and imported legacy particle pack for gun vfx. Also imported TextMeshPro for ammo counter and other possible UI elements.

---
+++ Ice Rink/Assets/Scripts/GunSystem.cs
@@ -0,0 +1,111 @@
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GunSystem : MonoBehaviour
+{
+    //Gun stats
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //references
+    public Camera fpsCam;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+
+    //Graphics
+    public GameObject muzzleFlash, bulletHoleGraphic;
+    public TextMeshProUGUI text;
+
+
+    private void MyInput()
+    {
+        //checks if allowed to hold mouse button or only tap, useful for different guns
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+
+        //shoot if not reloading and has bullets
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //bullet spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //calculate Direction with Spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+        //raycast
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
+        {
+            Debug.Log(rayHit.collider.name);
+
+            //if (rayHit.collider.CompareTag("Enemy"))
+                //rayHit.collider.GetComponent<ShootingAi>().TakeDamage(damage);
+        }
+        
+        //Graphics
+        Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
+        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        
+        bulletsLeft--;
+        bulletsShot--;
+
+        Invoke("ResetShot", timeBetweenShooting);
+
+        if(bulletsShot  > 0 && bulletsLeft >0)
+        Invoke("Shoot", timeBetweenShots);
+    }
+
+
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+
+    void Start()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot= true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyInput();
+
+        //ammo counter display
+        text.SetText(bulletsLeft + " / " + magazineSize);
+    }
+}
